1af00fb99f3bc1daf4c13f75d2803de221dd49b1
19025782 <19025782@DESKTOP-4OCCBNM>
2020-08-19 14:22:31 +0800

    edit

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
 
-public class C206_CaseStudy {
+public class C206_CaseStudy  {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<bid> bidList = new ArrayList<bid>();
+		bidList.add(new bid(0001,"Apple Watch","John34@gmail.com",null, 0.0 ));
+		bidList.add(new bid(0035,"Off-brand Speaker ","Johnson314@gmail.com",null, 0.0 ));
+		bidList.add(new bid(1256,"6 can cokes ","Harry13@gmail.com",null, 0.0 ));
+		bidList.add(new bid(4622,"Deck of cards ","Joe3333@gmail.com",null, 0.0 ));
 		int option = 0;
 
 		while (option != 5) {
@@ -12,37 +17,70 @@ public class C206_CaseStudy {
 			option = Helper.readInt("Enter an option > ");
 
 			if (option == 1) {
-				// View all items
+				// 
 				
 
 			} else if (option == 2) {
-				// Add a new item
+				// 
 				
 
 			} else if (option == 3) {
-				// Loan item
+				System.out.println("Option 1: Add bid");
+				System.out.println("Option 2: View bids");
+				System.out.println("Option 3: Delete bid");
+				int id = Helper.readInt("Enter an option > ");
+				if (id == 1) {
+					C206_CaseStudy.addbidlist(bidList);
+				}
+				else if (id == 2) {
+					C206_CaseStudy.viewbidList(bidList);
+				}
+				else if (id == 3) {
+					C206_CaseStudy.deletebidlist(bidList);
+				}
+				else {
+					System.out.println("Option not recognized ");
+				}
+					
 				
 
 			} else if (option == 4) {
-				// Return item
+				// 
 				
 			} else if (option == 5) {
 				System.out.println("Bye!");
 			} else {
 				System.out.println("Invalid option");
 			}
+		}
 
 		}
-	}
+	
 		public static void menu() {
 			System.out.println("Application");
-			System.out.println("1. Add User Account");
-			System.out.println("2. Add item");
-			System.out.println("3. Add bid");
-			System.out.println("4. Add deal");
+			System.out.println("1. User Account");
+			System.out.println("2. item");
+			System.out.println("3. Bid");
+			System.out.println("4. Deal");
 			System.out.println("5. Quit");
 			Helper.line(80, "-");
 
+		}
+		public static void viewbidList(ArrayList<bid> bidlist) {
+			int i = 0;
+			String output = String.format("%-10s %-30s %-10s %-10s %-20s\n", "Bid Id", "Item Name",
+					"Seller Email", "Buyer Email","Bid Price");
+			System.out.println(output);
+			while (i != bidlist.size()){
+				System.out.println(bidlist.get(i));
+				i++;
+			}
+		}
+		public static void addbidlist (ArrayList<bid> bidlist) {
+			//
+		}
+		public static void deletebidlist (ArrayList<bid> bidlist) {
+			//
 		}
 
 	
--- C206_CaseStudy/src/bid.java
+++ C206_CaseStudy/src/bid.java
@@ -6,7 +6,7 @@ public class bid {
 	private String seller_email;
 	private String buyer_email;
 	
-	public void String(int bid_id, String item_name, String seller_email, String buyer_email, double bid_price) {
+	public bid(int bid_id, String item_name, String seller_email, String buyer_email, double bid_price) {
 		this.bid_id = bid_id;
 		this.item_name = item_name;
 		this.seller_email = seller_email;
@@ -19,14 +19,29 @@ public class bid {
 	public int getbid_id() {
 		return bid_id;
 	}
-	public String item_name() {
+	public String getitem_name() {
 		return item_name;
 	}
-	public String buyer_email() {
+	public String getbuyer_email() {
 		return buyer_email;
 	}
-	public String seller_email() {
+	public String getseller_email() {
 		return seller_email;
+	}
+	public void setitem_name(String item_name) {
+		this.item_name = item_name;
+	}
+	public void setbuyer_email(String buyer_email) {
+		this.buyer_email = buyer_email;
+	}
+	public void setseller_email(String seller_email) {
+		this.seller_email = seller_email;
+	}
+	public void setbid_price(double bid_price) {
+		this.bid_price = bid_price;
+	}
+	public void setbid_id(int bid_id) {
+		this.bid_id = bid_id;
 	}
 
 
